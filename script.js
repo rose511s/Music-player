fc1a4db66f3e48dd8d9905f4f57aa4dc9b014d2a
Kratika Singh <kratikasingh397@gmail.com>
2024-08-24 23:40:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Cache DOM
+var audio = document.getElementById("music");
+var time = document.querySelector(".time");
+
+// Bind events
+document.querySelector(".play").addEventListener("click", play);
+document.querySelector(".pause").addEventListener("click", pause);
